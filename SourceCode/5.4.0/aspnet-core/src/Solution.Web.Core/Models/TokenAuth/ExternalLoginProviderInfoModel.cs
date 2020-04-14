ccc96a4d74a545bb7fe96bdc5ee32a6cbab8b53c
yuxianye <yuxianye@163.com>
2020-04-14 10:20:22 +0800

    增加源代码5.4

---
+++ SourceCode/5.4.0/aspnet-core/src/Solution.Web.Core/Models/TokenAuth/ExternalLoginProviderInfoModel.cs
@@ -0,0 +1,13 @@
+﻿using Abp.AutoMapper;
+using Solution.Authentication.External;
+
+namespace Solution.Models.TokenAuth
+{
+    [AutoMapFrom(typeof(ExternalLoginProviderInfo))]
+    public class ExternalLoginProviderInfoModel
+    {
+        public string Name { get; set; }
+
+        public string ClientId { get; set; }
+    }
+}
---
+++ SourceCode/5.4.0/aspnet-core/src/Solution.Web.Core/SolutionWebCoreModule.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Abp.AspNetCore;
+using Abp.AspNetCore.Configuration;
+using Abp.AspNetCore.SignalR;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Abp.Zero.Configuration;
+using Solution.Authentication.JwtBearer;
+using Solution.Configuration;
+using Solution.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
+namespace Solution
+{
+    [DependsOn(
+         typeof(SolutionApplicationModule),
+         typeof(SolutionEntityFrameworkModule),
+         typeof(AbpAspNetCoreModule)
+        ,typeof(AbpAspNetCoreSignalRModule)
+     )]
+    public class SolutionWebCoreModule : AbpModule
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public SolutionWebCoreModule(IWebHostEnvironment env)
+        {
+            _env = env;
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        public override void PreInitialize()
+        {
+            Configuration.DefaultNameOrConnectionString = _appConfiguration.GetConnectionString(
+                SolutionConsts.ConnectionStringName
+            );
+
+            // Use database for language management
+            Configuration.Modules.Zero().LanguageManagement.EnableDbLocalization();
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(SolutionApplicationModule).GetAssembly()
+                 );
+
+            ConfigureTokenAuth();
+        }
+
+        private void ConfigureTokenAuth()
+        {
+            IocManager.Register<TokenAuthConfiguration>();
+            var tokenAuthConfig = IocManager.Resolve<TokenAuthConfiguration>();
+
+            tokenAuthConfig.SecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appConfiguration["Authentication:JwtBearer:SecurityKey"]));
+            tokenAuthConfig.Issuer = _appConfiguration["Authentication:JwtBearer:Issuer"];
+            tokenAuthConfig.Audience = _appConfiguration["Authentication:JwtBearer:Audience"];
+            tokenAuthConfig.SigningCredentials = new SigningCredentials(tokenAuthConfig.SecurityKey, SecurityAlgorithms.HmacSha256);
+            tokenAuthConfig.Expiration = TimeSpan.FromDays(1);
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(SolutionWebCoreModule).GetAssembly());
+        }
+
+        public override void PostInitialize()
+        {
+            IocManager.Resolve<ApplicationPartManager>()
+                .AddApplicationPartsIfNotAddedBefore(typeof(SolutionWebCoreModule).Assembly);
+        }
+    }
+}
